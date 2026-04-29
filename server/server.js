782b29e529e0e662fa7e59c7e8dc7070b7f1be3e
Shubham Rawat <rawatshubam269@gmail.com>
2026-04-29 13:32:56 +0530

    Updated : mongodb connected success

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 import app from "./app.js";
 import dotenv from "dotenv";
-// import connectDB from "./config/connectDB.js";
+import connectDB from "./config/connectDB.js";
 dotenv.config();
 
-// connectDB();
+connectDB();
 
 const PORT = process.env.PORT || 8082;
 
