46ae25819113069d8daa6ed14034d1b888ada2f3
Shubham Rawat <rawatshubam269@gmail.com>
2026-04-30 20:51:25 +0530

    Added : project navigation done

--- client/src/components/custom/ProjectCard.tsx
+++ client/src/components/custom/ProjectCard.tsx
@@ -1,13 +1,22 @@
 import { Card, CardContent } from "@/components/ui/card";
 import type { Project } from "@/types/project";
+import { useNavigate } from "react-router-dom";
 
 interface Props {
   project: Project;
 }
 
 export default function ProjectCard({ project }: Props) {
+  const navigate = useNavigate();
+
+  function handleNaviateToProjectDetail(id: string) {
+    navigate(`/project/${id}`);
+  }
   return (
-    <Card className="hover:shadow-lg transition cursor-pointer">
+    <Card
+      className="hover:shadow-lg transition cursor-pointer"
+      onClick={() => handleNaviateToProjectDetail(project._id)}
+    >
       <CardContent className="p-4">
         <h2 className="text-lg font-semibold">{project.name}</h2>
 
